359171842fbc8be5a858a12100ba48795648b15b
E3tech Interns <e3techllpInterns@outlook.com>
2020-02-16 20:37:38 +0530

    nothing

--- Wt latest/Authentication.js
+++ Wt latest/Authentication.js
@@ -1,7 +1,7 @@
 function login() {
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
   .then(function() {
-    
+
     var email = document.getElementById("useremail").value;
     var password = document.getElementById("password").value;
     return firebase.auth().signInWithEmailAndPassword(email, password);
@@ -16,7 +16,7 @@ firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
 
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      window.location.href = "homepage.html";
+    //  window.location.href = "homepage.html";
     } else {
       // No user is signed in.
     }
